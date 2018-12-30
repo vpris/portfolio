45e0d412838c50edc604eecf09a195b7368bdee5
vpris <vpris@MacBook-Pro-vpris.local>
2018-12-31 03:37:42 +0600

    Add typed text

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,15 @@ $(document).ready(function () {
         animation: 'fade',
         play: 5000,
         pagination: false,
-
     });
 
+    let typed = new Typed(".typed", {
+        strings: ["Frontend Web Developer", "HTML5/CSS3/a little JS", "Student at Udemy and Geekbrains"],
+        typeSpeed: 75,
+        loop: true,
+        startDelay: 1500,
+        showCursor: false,
+        backSpeed: 70,
+        loopCount: false
+    });
 });
